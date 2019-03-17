5325e9a39e274086c81d26ee02a2a754af3f3c0a
peppato <cfcardillo23@gmail.com>
2019-03-17 04:55:23 -0400

    adding sr_cleanup

--- R/cleanup.R
+++ R/cleanup.R
@@ -22,3 +22,80 @@ sr_trim_headers <- function(df, row_number){
   df
   
 }
+
+
+
+#' Utility function for renaming columns to output column names and reducing dataframes down to the desired columns in the mapping
+#'
+#' @importFrom dplyr mutate_if bind_cols %>%
+#' @importFrom tidyr drop_na
+#'
+#' @param df a trimmed dataframe - colnames are the actual colnames
+#' @param source the determined input source of the dataframe
+#' @param mapping the mapping dataframe in its original form
+#'
+#' @return a dataframe with the desired columns
+#'
+#' @examples
+#' sr_trim_headers(my_df, 4)
+#'
+
+sr_rename_columns <- function(df, source, mapping){
+  
+  map_cols <- c("output", source)
+  
+  colnames(df) <- colnames(df) %>% sr_clean_colnames()
+  
+  map_with_na <- mapping[, map_cols]
+  
+  clean_map <- map_with_na %>% 
+    tidyr::drop_na() %>%
+    mutate_if(is.character, tolower) %>%
+    mutate_if(is.character, trimws)
+  
+  col_list <- list()
+  
+  for (i in 1:nrow(clean_map)){
+    
+    old_col <- clean_map[[i, source]]
+    new_col <- clean_map[[i, "output"]]
+    
+    temp_df <- tibble(df[, old_col])
+    colnames(temp_df) = new_col
+    
+    col_list[[i]] <- temp_df
+
+  }
+  
+  bind_cols(col_list)
+  
+}
+
+
+#' User function for cleanup
+#'
+#' @importFrom dplyr mutate select %>%
+#' @importFrom purrr map2
+#'
+#' @param matched_files_list the matched file list from sr_match
+#' @param mapping the mapping dataframe in its original form
+#'
+#' @return a dataframe with cleaned nested dataframes ready for compilation
+#'
+#' @examples
+#' sr_cleanup(my_df, my_mapping)
+#'
+#' @export
+
+sr_cleanup <- function(matched_files_list, mapping){
+  
+  matched_files_list$matched_files %>%
+    mutate(sr_trimmed = map2(sr_data, header_row, sr_trim_headers),
+           sr_renamed = map2(sr_trimmed, sr_source, sr_rename_columns, mapping),
+           sr_ready = sr_renamed) %>%
+    select(sr_filename, sr_source, sr_ready)
+    
+  
+}
+
+
